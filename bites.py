f83a714bb2af2a88593cf2458aad33a18ed62b3b
John Baxter <john.g.baxter@googlemail.com>
2020-09-06 13:25:20 +0100

    add move_ant function and corresponding five passing tests

--- bites.py
+++ bites.py
@@ -6,13 +6,33 @@ def initialise_ants(ants):
     ant_positions[ant] = None
   return ant_positions
   
-def initialise_path(foods):
-  path = []
+def initialise_trail(foods):
+  trail = []
   for food, amount in foods.items():
-    path = path + ([food] * amount)
-  random.shuffle(path)
-  return path
+    trail = trail + ([food] * amount)
+  random.shuffle(trail)
+  return trail
 
 def initialise_hand():
   hand = {}
   return hand
+
+def move_ant(trail, ant_positions, ant):
+  colour_matching_food = {
+    "purple": "grapes",
+    "red": "apple",
+    "brown": "bread",
+    "yellow": "cheese",
+    "green": "pepper"}
+  if ant_positions[ant] is None:
+    ant_positions[ant] = 0
+  else:
+    ant_positions[ant] += 1
+
+  if trail[ant_positions[ant]] == colour_matching_food[ant]:
+    return ant_positions
+  else:
+    while trail[ant_positions[ant]] != colour_matching_food[ant]:
+      ant_positions[ant] += 1
+
+  return ant_positions
--- bites_test.py
+++ bites_test.py
@@ -1,7 +1,8 @@
 import unittest
 from bites import initialise_ants
-from bites import initialise_path
+from bites import initialise_trail
 from bites import initialise_hand
+from bites import move_ant
 
 class InitialiseAntsTest(unittest.TestCase):
   # test 1
@@ -31,40 +32,40 @@ class InitialiseAntsTest(unittest.TestCase):
     actual_ant_positions = initialise_ants(ants)
     self.assertEqual(expected_ant_positions, actual_ant_positions)
 
-class InitialisePathTest(unittest.TestCase):
+class InitialiseTrailTest(unittest.TestCase):
   # test 4
-  def test_can_initialise_path_with_one_token(self):
+  def test_can_initialise_trail_with_one_token(self):
     foods = {"apple": 1}
-    expected_path = ["apple"]
-    actual_path = initialise_path(foods)
-    self.assertEqual(expected_path, actual_path)
+    expected_trail = ["apple"]
+    actual_trail = initialise_trail(foods)
+    self.assertEqual(expected_trail, actual_trail)
 
-  def test_can_initialise_path_with_two_different_tokens(self):
+  def test_can_initialise_trail_with_two_different_tokens(self):
   # test 5
     foods = {"apple": 1, "grapes": 1}
-    expected_paths = [["apple", "grapes"], ["grapes", "apple"]]
-    actual_path = initialise_path(foods)
-    self.assertIn(actual_path, expected_paths)
+    expected_trails = [["apple", "grapes"], ["grapes", "apple"]]
+    actual_trail = initialise_trail(foods)
+    self.assertIn(actual_trail, expected_trails)
 
-  def test_can_initialise_path_with_five_of_same_token(self):
+  def test_can_initialise_trail_with_five_of_same_token(self):
   # test 6
     foods = {"apple": 5}
-    expected_path = ["apple", "apple", "apple", "apple", "apple"]
-    actual_path = initialise_path(foods)
-    self.assertEqual(expected_path, actual_path)
+    expected_trail = ["apple", "apple", "apple", "apple", "apple"]
+    actual_trail = initialise_trail(foods)
+    self.assertEqual(expected_trail, actual_trail)
 
   def test_can_shuffle_two_plus_one_tokens(self):
   # test 7
     foods = {"apple": 2, "grapes": 1}
-    expected_paths = [
+    expected_trails = [
       ["grapes", "apple", "apple"],
       ["apple", "grapes", "apple"],
       ["apple", "apple", "grapes"]
       ]
-    actual_path = initialise_path(foods)
-    self.assertIn(actual_path, expected_paths)
+    actual_trail = initialise_trail(foods)
+    self.assertIn(actual_trail, expected_trails)
 
-  def test_full_size_path_using_count_method_and_length(self):
+  def test_full_size_trail_using_count_method_and_length(self):
   # test 8
     foods = {
       "apple": 10,
@@ -73,15 +74,15 @@ class InitialisePathTest(unittest.TestCase):
       "pepper": 10,
       "bread": 10
     }
-    expected_path_length = 50
-    path = initialise_path(foods)
-    actual_path_length = len(path)
-    self.assertEqual(actual_path_length, expected_path_length)
-    self.assertEqual(path.count("apple"), 10)
-    self.assertEqual(path.count("grapes"), 10)
-    self.assertEqual(path.count("cheese"), 10)
-    self.assertEqual(path.count("pepper"), 10)
-    self.assertEqual(path.count("bread"), 10)
+    expected_trail_length = 50
+    trail = initialise_trail(foods)
+    actual_trail_length = len(trail)
+    self.assertEqual(actual_trail_length, expected_trail_length)
+    self.assertEqual(trail.count("apple"), 10)
+    self.assertEqual(trail.count("grapes"), 10)
+    self.assertEqual(trail.count("cheese"), 10)
+    self.assertEqual(trail.count("pepper"), 10)
+    self.assertEqual(trail.count("bread"), 10)
 
 class InitialiseHandTest(unittest.TestCase):
   def test_can_initialise_player_hand(self):
@@ -90,7 +91,46 @@ class InitialiseHandTest(unittest.TestCase):
     self.assertIsInstance(hand, dict)
     self.assertEqual(len(hand), 0)
 
+class MoveAntTest(unittest.TestCase):
+  def test_can_move_onto_trail_of_length_one(self):
+  # test 10
+    ant_positions = {"red": None}
+    trail = ["apple"]
+    expected_new_ant_positions = {"red": 0}
+    actual_new_ant_positions = move_ant(trail, ant_positions, "red")
+    self.assertEqual(actual_new_ant_positions, expected_new_ant_positions)
 
+  def test_can_move_onto_adjacent_position(self):
+  # test 11
+    ant_positions = {"red": 0}
+    trail = ["apple", "apple"]
+    expected_new_ant_positions = {"red": 1}
+    actual_new_ant_positions = move_ant(trail, ant_positions, "red")
+    self.assertEqual(actual_new_ant_positions, expected_new_ant_positions)
+
+  def test_ant_moves_past_wrong_food_tokens(self):
+  # test 12
+    ant_positions = {"red": 0}
+    trail = ["apple", "grapes", "apple"]
+    expected_new_ant_positions = {"red": 2}
+    actual_new_ant_positions = move_ant(trail, ant_positions, "red")
+    self.assertEqual(actual_new_ant_positions, expected_new_ant_positions)
+
+  def test_ant_moves_past_missing_food_tokens(self):
+  # test 13
+    ant_positions = {"red": 0}
+    trail = ["apple", None, "apple"]
+    expected_new_ant_positions = {"red": 2}
+    actual_new_ant_positions = move_ant(trail, ant_positions, "red")
+    self.assertEqual(actual_new_ant_positions, expected_new_ant_positions)
+
+  def test_only_intended_ant_moves(self):
+  # test 14
+    ant_positions = {"red": 0, "purple": 1}
+    trail = ["apple", "grapes", "apple"]
+    expected_new_ant_positions = {"red": 2, "purple": 1}
+    actual_new_ant_positions = move_ant(trail, ant_positions, "red")
+    self.assertEqual(actual_new_ant_positions, expected_new_ant_positions)
 
 if __name__ == '__main__':
   unittest.main()
