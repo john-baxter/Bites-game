33a46cbf54faa0ff633e06b27aa9f38c9edbbe6a
John Baxter <john.g.baxter@googlemail.com>
2020-09-08 17:25:27 +0100

    Add internal documentation to each method in player.py. All internal documentation to each constant in constants.py.

--- constants.py
+++ constants.py
@@ -1,6 +1,20 @@
 ANTS = ['purple', 'red', 'brown', 'yellow', 'green']
+"""A list of ID for the insect meeple
+
+The ants will be identified by their colour.
+The order in this list is not important
+Each element is a string
+"""
 
 FOOD_TYPES = ['grapes', 'apple', 'bread', 'cheese', 'pepper']
+"""A list of the types of token used for the trail
+
+The food tokens will be labelled according to the name of the food idem depicted.
+The order in this list is not important.
+Each name is written in singular form except 'grapes'; this is in reflection of the 
+real game pieces.
+Each element is a string
+"""
 
 K_COLOUR_V_FOOD_DICT = {
   "purple": "grapes",
@@ -8,5 +22,21 @@ K_COLOUR_V_FOOD_DICT = {
   "brown": "bread",
   "yellow": "cheese",
   "green": "pepper"}
+"""Cross reference which food token matches with which ant colour
+
+A dictionary to refer to when cross referencing is required between 
+ant colour and food type.
+Keys are ant colours as strings
+Values are food IDs as strings
+"""
 
 K_FOOD_V_COLOUR_DICT = dict((v, k) for k, v in K_COLOUR_V_FOOD_DICT.items())
+"""Cross reference which ant colour matches with which food token
+
+A dictionary to refer to when cross referencing is required between 
+food type and ant colour.
+This contains the same pairs as K_COLOUR_V_FOOD_DICT with the keys 
+and values interchanged.
+Keys are food IDs as strings.
+Values are ant colours as strings.
+"""
--- player.py
+++ player.py
@@ -3,20 +3,75 @@ from constants import K_FOOD_V_COLOUR_DICT
 
 class Player():
   def __init__(self):
+    """Initialises an instance of the Player class
+
+    Attributes
+    ----------
+    hand : (dict)
+      An empty dictionary ready to be populated with the food tokens collected by the player during the game
+      keys will be the ID of the food as strings
+      values will be the number of that food as integers
+
+    score : (integer)
+      A record of the points scored by this player
+    """
     self.hand = self.initialise_hand()
     self.score = 0
 
   def initialise_hand(self):
+    """Generates the player's hand at the start of the game
+    The hand will be empty, ready to be populated with food tokens as 
+    the player collects them through the game.
+
+    Returns
+    -------
+    hand : (dict)
+      An empty dictionary ready to be populated with the food tokens collected by the player during the game
+      keys will be the ID of the food as strings
+      values will be the number of that food as integers
+    """
     hand = {}
     return hand
 
   def store_food(self, food):
+    """Places collected token into player's hand
+    Increments the number of a given food type; or if not already present, 
+    adds food type to hand.
+
+    Updates
+    -------
+    hand : (dict)
+      Dictionary of all food tokens held by the player at tyhe current time. 
+      will be used to calculate score at end of game.
+      Keys are food types as strings.
+      Values are integers.
+    """
     if food in self.hand:
       self.hand[food] += 1
     else:
       self.hand[food] = 1
     
   def score_hand(self, anthill):
+    """Calculates player's points at the end of the game
+    Could be called any time but to reflect the real game there is no running 
+    total of points; just a single calculation and comparison after the final ant 
+    has reached the anthill.
+
+    Arguments
+    ---------
+    anthill : (list)
+      A list recording which ant is in which spot on the anthill at the end of the game.
+      The inxex of each ant will be used as the value of each of the corresponding 
+      food token in a player's hand.
+      Each element is a string
+
+    
+    Updates
+    -------
+    score : (integer)
+      An integer  showing the player's points total. Will be initialised at 0 and 
+      will be updated  at the end of the game.
+    """
     self.score = 0
     for food in self.hand:
       self.score += anthill.index(K_FOOD_V_COLOUR_DICT[food]) * self.hand[food]
