088bf5fcf6f17eeda4d5e5ab659a147cd9b292b3
John Baxter <john.g.baxter@googlemail.com>
2020-09-24 21:58:10 +0100

    WIP make_move; COP commit

--- player.py
+++ player.py
@@ -374,7 +374,17 @@ class Player():
     """
     allowed_choices_direction = []
     
+    """*********************************************
+    """
+    print("************************")
+    print(ant_positions)
+    print(ant)
+    print(trail)
     trail_slice_pre_ant = trail[:ant_positions[ant]]
+
+    """*********************************************
+    """
+
     trail_slice_post_ant = trail[ant_positions[ant]+1:]
 
     for food in trail_slice_post_ant:
@@ -394,11 +404,35 @@ class Player():
     return allowed_choices_direction
 
   def take_turn(self, trail, ant_positions, anthill):
-    self.allowed_choices_ants = self.define_allowed_choices_ants(ant_positions)
-    self.ant = self.make_choice(self.allowed_choices_ants, PROMPT_TEXT_ANT_CHOICE)
-    self.anthill_antpositions_tuple = self.move_ant(trail, ant_positions, anthill, self.ant)
-    self.allowed_choices_direction = self.define_allowed_choices_direction(self.ant, trail, ant_positions)
-    self.direction = self.make_choice(self.allowed_choices_direction, PROMPT_TEXT_DIRECTION_CHOICE)
-    self.foodtohand_trail_tuple = self.take_food_from_trail(trail, ant_positions, self.ant, self.direction)
-    self.store_food(self.foodtohand_trail_tuple[0])
-    return (self.foodtohand_trail_tuple[1], ant_positions)
+    allowed_choices_ants = self.define_allowed_choices_ants(ant_positions)
+    ant = self.make_choice(allowed_choices_ants, PROMPT_TEXT_ANT_CHOICE)
+    (anthill, ant_positions) = self.move_ant(trail, ant_positions, anthill, ant)
+
+    """
+
+    if self.goes_to_antill():
+      self.move_to_anthill()
+      self.define_food_choices()
+      self.take_food_from_anthill()
+      #self.store_food()
+    else:
+      self.move_to_trail()
+      self.define_direction_choices()
+      self.make_choice()
+      self.take_food()
+      #self.store_food()
+    self.store_food()
+
+    """
+    
+    """******************************************************
+    """
+    # Put IF statement to account for the fact there is no choice of direction when ant is (ie has just moved) on the anthill
+    allowed_choices_direction = self.define_allowed_choices_direction(ant, trail, ant_positions)
+
+    """******************************************************
+    """
+    direction = self.make_choice(allowed_choices_direction, PROMPT_TEXT_DIRECTION_CHOICE)
+    (food_to_hand, trail) = self.take_food_from_trail(trail, ant_positions, ant, direction)
+    self.store_food(food_to_hand)
+    return (trail, ant_positions)
--- player_test.py
+++ player_test.py
@@ -609,5 +609,32 @@ class TakeTurnTest(unittest.TestCase):
     self.assertEqual(mario.hand, expected_new_hand)
     input_patcher.stop()
 
+  def test_single_ant_moves_from_centre_of_three_element_trail_onto_anthill(self):
+  # test 67
+    # Given
+    anthill = [None]
+    mario = Player("mario")
+    mario.hand = {"cheese": 1}
+    trail = ["pepper", "apple", None]
+    ant_positions = {"red": 1}
+    input_patcher = mock.patch('builtins.input', return_value = "red")
+    input_mock = input_patcher.start()
+    # When
+    (actual_new_trail, actual_new_ant_positions, actual_new_anthill) = \
+      mario.take_turn(trail, ant_positions, anthill)
+    # Then
+    expected_new_trail = trail
+    expected_new_ant_positions = {"red": "anthill"}
+    expected_new_anthill = ["red"]
+    # self.assertEqual(mario.take_turn(trail, ant_positions, anthill), (expected_new_trail, expected_new_ant_positions, expected_new_anthill))
+    self.assertEqual(actual_new_trail, expected_new_trail)
+    self.assertEqual(actual_new_ant_positions, expected_new_ant_positions)
+    self.assertEqual(actual_new_anthill, expected_new_anthill)
+    self.assertEqual(mario.hand, {"cheese": 1})
+    input_patcher.stop()
+
+
+
+
 if __name__ == '__main__':
   unittest.main(verbosity = 2)
