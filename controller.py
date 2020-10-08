dacf5e8cbdd662e701f425eccbe3a0685ff621a1
John Baxter <john.g.baxter@googlemail.com>
2020-10-08 17:08:59 +0100

    Add second passing test, T100, for enter_number_of_players(). Function now rejects input = 1 then returns what is entered next.

--- controller.py
+++ controller.py
@@ -1,3 +1,6 @@
 def enter_number_of_players():
   number_of_players = input()
+  if number_of_players == 1:
+    number_of_players = input()
+
   return number_of_players
--- controller_test.py
+++ controller_test.py
@@ -13,6 +13,15 @@ class GetPlayerInfoTest(unittest.TestCase):
     self.assertEqual(input_mock.call_count, 1)
     input_patcher.stop()
 
+  def test_enter_number_of_players_does_not_allow_1_player(self):
+    # test 100
+    expected_result = 2
+    input_patcher = mock.patch('builtins.input', side_effect = [1, 2])
+    input_mock = input_patcher.start()
+    actual_result = controller.enter_number_of_players()
+    self.assertEqual(actual_result, expected_result)
+    self.assertEqual(input_mock.call_count, 2)
+    input_patcher.stop()
 
 
 
