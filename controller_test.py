44a22a4c21b1ee99e294f4f529b4669c84396f6a
John Baxter <john.g.baxter@googlemail.com>
2020-10-12 14:27:14 +0100

    Update tests in EnterNumberOfPlayersTest so that return_value for input_mock is type string (eg change 2 to '2') Now correctly resembles the real return value of input() method.

--- controller_test.py
+++ controller_test.py
@@ -11,7 +11,7 @@ class EnterNumberOfPlayersTest(unittest.TestCase):
   # test 99
   def test_enter_number_of_players_returns_number_entered(self):
     expected_result = 2
-    input_patcher = mock.patch('builtins.input', return_value = 2)
+    input_patcher = mock.patch('builtins.input', return_value = '2')
     input_mock = input_patcher.start()
     actual_result = enter_number_of_players()
     self.assertEqual(actual_result, expected_result)
@@ -21,7 +21,7 @@ class EnterNumberOfPlayersTest(unittest.TestCase):
   def test_enter_number_of_players_does_not_allow_1_player(self):
     # test 100
     expected_result = 2
-    input_patcher = mock.patch('builtins.input', side_effect = [1, 2])
+    input_patcher = mock.patch('builtins.input', side_effect = ['1', '2'])
     input_mock = input_patcher.start()
     actual_result = enter_number_of_players()
     self.assertEqual(actual_result, expected_result)
@@ -31,7 +31,7 @@ class EnterNumberOfPlayersTest(unittest.TestCase):
   def test_enter_number_of_players_does_not_allow_6_players(self):
     # test 101
     expected_result = 2
-    input_patcher = mock.patch('builtins.input', side_effect = [6, 2])
+    input_patcher = mock.patch('builtins.input', side_effect = ['6', '2'])
     input_mock = input_patcher.start()
     actual_result = enter_number_of_players()
     self.assertEqual(actual_result, expected_result)
@@ -41,7 +41,7 @@ class EnterNumberOfPlayersTest(unittest.TestCase):
   def test_check_no_error_if_input_is_not_an_int_player_can_enter_again(self):
     # test 102
     expected_result = 2
-    input_patcher = mock.patch('builtins.input', side_effect = ["two", 2])
+    input_patcher = mock.patch('builtins.input', side_effect = ["two", '2'])
     input_mock = input_patcher.start()
     print_patcher = mock.patch('builtins.print')
     print_mock = print_patcher.start()
@@ -53,7 +53,7 @@ class EnterNumberOfPlayersTest(unittest.TestCase):
 
   def test_user_is_prompted_to_enter_number_of_players(self):
     # test 103
-    input_patcher = mock.patch('builtins.input', return_value = 2)
+    input_patcher = mock.patch('builtins.input', return_value = '2')
     input_mock = input_patcher.start()
     enter_number_of_players()
     input_mock.assert_called_once_with("Please enter the number of players: ")
