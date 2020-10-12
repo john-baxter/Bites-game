a4cb3013f16b3a1b11a3a984414657cc882d319d
John Baxter <john.g.baxter@googlemail.com>
2020-10-12 13:19:27 +0100

    Update to controller.enter_number_of_players() to convert input into int. Error discovered during feature-testing. No update to test files.

--- controller.py
+++ controller.py
@@ -6,7 +6,7 @@ from bites import Bites
 def enter_number_of_players():
   number_of_players = 0
   while number_of_players not in range(MIN_PLAYERS, MAX_PLAYERS+1):
-    number_of_players = input("Please enter the number of players: ")
+    number_of_players = int(input("Please enter the number of players: "))
   return number_of_players
 
 def generate_player():
@@ -26,5 +26,6 @@ def start_new_game():
   play_bites = Bites(ANTS, TOKENS_FOR_TRAIL, players)
   play_bites.play_full_game()
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
   # play_bites = Bites(ANTS, TOKENS_FOR_TRAIL, prepare_list_of_players())
+  start_new_game()
