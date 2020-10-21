f3210ccff4db8d3ace60ccd7f63a0339aa70bb74
John Baxter <john.g.baxter@googlemail.com>
2020-10-21 18:19:27 +0100

    Redo previous commit due to save error.

--- player_test.py
+++ player_test.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest import mock
 from player import Player
-# from controller import show_allowed_choices_from_list
 from constants import ANTHILL_CARD_DICT
 
 class PlayerInitTest(unittest.TestCase):
